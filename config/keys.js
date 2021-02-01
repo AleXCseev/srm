3843de5dcc4e020e30f437dec3e1f3bb3dcd30e1
Konstantin Alekseev <alexcseev1990@gmail.com>
2021-02-01 16:30:34 +0200

    add models and controllers

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI:
+		"mongodb+srv://AleXCseev:01091990q@cluster0.ie4ye.mongodb.net/MEANdb?retryWrites=true&w=majority",
+};
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,35 @@
-module.exports.login = function(req, res) {
-  res.status(200).json({
-    login: "from controller"
-  })
-}
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-module.exports.register = function(req, res) {
-  res.status(200).json({
-    register: "from controller"
-  })
-}
+module.exports.login = function (req, res) {
+	res.status(200).json({
+		login: {
+			email: req.body.email,
+			password: req.body.password,
+		},
+	});
+};
+
+module.exports.register = async function (req, res) {
+	const candidate = await User.findOne({ email: req.body.email });
+
+	if (candidate) {
+		res.status(409).json({
+			message: "Такой email уже существует",
+		});
+	} else {
+		const salt = bcrypt.genSaltSync(10);
+		const password = req.body.password;
+		const user = new User({
+			email: req.body.email,
+			password: bcrypt.hashSync(password, salt),
+		});
+
+		try {
+			await user.save();
+			res.status(201).json(user);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
